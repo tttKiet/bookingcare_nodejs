3b0285fe26b8c9e51d86cd7efaa4535d63eed5d0
BuiKiet <roiyano1utl@gmail.com>
2024-08-04 13:20:55 +0700

    * edit - build index.js cors

--- dist/index.js
+++ dist/index.js
@@ -9,20 +9,30 @@ var _db = _interopRequireDefault(require("./config/db"));
 var _routes = _interopRequireDefault(require("./routes"));
 var _cors = _interopRequireDefault(require("cors"));
 var _middleWares = require("./middleWares");
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
 var app = (0, _express["default"])();
 
 // Logger browser
 app.use((0, _morgan["default"])("dev"));
 var corsOptions = {
-  origin: ["http://127.0.0.1:3000", "https://sandbox.vnpayment.vn", "*"],
-  credentials: true
+  origin: [
+    "http://127.0.0.1:3000",
+    "https://sandbox.vnpayment.vn",
+    "https://bookingcare-nextjs-e2hsxu9lb-tttkiets-projects.vercel.app/",
+    "https://bookingcare-nextjs.vercel.app/",
+    "https://bookingcare-nextjs.vercel.app",
+  ],
+  credentials: true,
 };
 app.use((0, _cors["default"])(corsOptions));
 app.use(_express["default"].json());
-app.use(_express["default"].urlencoded({
-  extended: false
-}));
+app.use(
+  _express["default"].urlencoded({
+    extended: false,
+  })
+);
 
 // Connect to database
 (0, _db["default"])();
@@ -34,7 +44,9 @@ app.use(_middleWares.handleError);
 app.use((0, _cookieParser["default"])());
 
 // Static files
-app.use(_express["default"]["static"](_path["default"].join(__dirname, "public")));
+app.use(
+  _express["default"]["static"](_path["default"].join(__dirname, "public"))
+);
 
 // Config routers
 (0, _routes["default"])(app);
